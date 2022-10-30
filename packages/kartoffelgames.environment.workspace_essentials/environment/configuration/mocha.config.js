c9e95254e58e5ae2a435de944b5ed61bae361e0c
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-10-30 02:14:03 +0200

    Remove unused code.

--- packages/kartoffelgames.environment.workspace_essentials/environment/configuration/mocha.config.js
+++ packages/kartoffelgames.environment.workspace_essentials/environment/configuration/mocha.config.js
@@ -1,17 +1,5 @@
 // @ts-check
 
-const gFilereader = require('fs');
-
-/**
- * Get project name.
- */
-const gGetProjectName = () => {
-    const lFileContent = gFilereader.readFileSync("package.json", 'utf8');
-    const lFileJson = JSON.parse(lFileContent);
-
-    return lFileJson.projectName;
-};
-
 module.exports = {
     "require": [
         "jsdom-global/register"
