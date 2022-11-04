c5912baabf0fef20a0956f6b6bfb86032f3556ff
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-11-05 00:36:37 +0100

    Add global executable command.

--- packages/kartoffelgames.environment/source/index.ts
+++ packages/kartoffelgames.environment/source/index.ts
@@ -1,7 +1,20 @@
 #! /usr/bin/env node
 
-// import { spawn} from 'child_process';
+import { spawn } from 'child_process';
 
-// console.log(process.argv);
+(() => {
+    // Find kg cli path.
+    const lKgCliPath: string = require.resolve('@kartoffelgames/environment.cli/library/source/cli.js');
 
-// var child = spawn('npx', ['--inspect-brk=3000', 'index.js'], { stdio: 'inherit' })
+    // Get all command parameter and find index of starting kg parameter.
+    const lCommandParameter: Array<string> = process.argv;
+    const lKgStartCommandIndex: number = lCommandParameter.findIndex((pParameter) => {
+        return pParameter.endsWith('index.js');
+    });
+
+    // Array of only kg parameters.
+    const lKgCommandParts: Array<string> = lCommandParameter.slice(lKgStartCommandIndex + 1);
+
+    // Execute command.
+    spawn('node', [lKgCliPath, ...lKgCommandParts], { stdio: 'inherit' });
+})();
