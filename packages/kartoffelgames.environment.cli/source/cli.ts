8b64bc652b3c4739d8cfd732c9a0532261efc552
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2022-11-08 00:08:32 +0100

    Small fixes (#32)
    * Sync packages.
    
    * Add debug information.
    
    * Add missing cli config.
    
    * Static root found.
    
    * Remove head.
    
    * Change search to execute.
    
    * Debug print project packages.
    
    * Faster client build by removing force.

--- packages/kartoffelgames.environment.cli/source/cli.ts
+++ packages/kartoffelgames.environment.cli/source/cli.ts
@@ -7,9 +7,6 @@ import { CliPackages } from './cli/cli-packages';
 (async () => {
     const lConsole: Console = new Console();
 
-    // Output main banner.
-    lConsole.banner('KG ENVIRONMENT');
-    
     // Construct paths.
     const lCurrentWorkingDirectoryPath: string = process.cwd();
 
@@ -19,14 +16,13 @@ import { CliPackages } from './cli/cli-packages';
     // Execute command.
     try {
         // Check for changed command root package.
-        let lCommandRootPackagePath: string = lCurrentWorkingDirectoryPath;
+        let lCommandRootPackagePath: string;
         const lCommandRootParameter = lParameter.parameter.get('command-root-package');
         if (lCommandRootParameter?.value) {
             lCommandRootPackagePath = lCommandRootParameter.value;
-            lConsole.write(`Changed command root: "${lCommandRootPackagePath}"\n`);
+        } else {
+            lCommandRootPackagePath = Project.findRoot(lCurrentWorkingDirectoryPath);
         }
-
-        lConsole.write('Search command...\n\n');
 
         // Init command indexing.
         const lCliPackagesHandler: CliPackages = new CliPackages(lCommandRootPackagePath);
@@ -46,13 +42,36 @@ import { CliPackages } from './cli/cli-packages';
         // Create package handler.
         const lProject: Project = new Project(lCurrentWorkingDirectoryPath, lDefaultConfiguration);
 
+        // Print debug information.
+        if (lParameter.parameter.has('debug')) {
+            lConsole.writeLine(`Project root: ${lProject.projectRootDirectory}`, 'green');
+
+            // Print all packages.
+            lConsole.writeLine(`CLI-Packages:`, 'green');
+            for (const lGroupName of Object.keys(lCliPackages)) {
+                lConsole.writeLine(`    ${lGroupName}:`, 'green');
+
+                const lPackages = lCliPackages[lGroupName];
+                for (const lPackage of lPackages) {
+                    lConsole.writeLine(`        ${lPackage}:`, 'green');
+                }
+            }
+
+            // Print all projects.
+            lConsole.writeLine(`Project-Packages:`, 'green');
+            for (const lProjectInformation of lProject.readAllProject()) {
+                lConsole.writeLine(`    ${lProjectInformation.packageName} -- ${lProjectInformation.version}`, 'green');
+            }
+        }
+
         // Execute command.
+        lConsole.write('Execute command...\n');
         await lCliCommandHandler.execute(lParameter, lProject);
     } catch (e) {
         lConsole.writeLine((<any>e).toString(), 'red');
 
         // Include error stack when command has --stack parameter. 
-        if (lParameter.parameter.has('stack')) {
+        if (lParameter.parameter.has('debug')) {
             lConsole.writeLine((<Error>e)?.stack ?? '', 'red');
         }
 
