2e3f59e4a1b1536a449c3b01f93c614bd95de59e
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2022-11-19 02:25:25 +0100

    Add page command.

---
+++ packages/kartoffelgames.environment.command_page/source/command.ts
@@ -0,0 +1,56 @@
+import { CliParameter, IKgCliCommand, KgCliCommandDescription } from '@kartoffelgames/environment.cli';
+import { Console, FileUtil, Project } from '@kartoffelgames/environment.core';
+import { KgCliCommand as BuildCommand } from '@kartoffelgames/environment.command-build';
+import * as path from 'path';
+
+
+export class KgCliCommand implements IKgCliCommand {
+    /**
+     * Command description.
+     */
+    public get information(): KgCliCommandDescription {
+        return {
+            command: {
+                description: 'Start page with local http server.',
+                pattern: 'page <package_name> --build-only'
+            },
+            configuration: {
+                name: 'page',
+                default: false
+            }
+        };
+    }
+
+    /**
+     * Execute command.
+     * @param pParameter - Command parameter.
+     * @param _pCliPackages - All cli packages grouped by type.
+     */
+    public async run(pParameter: CliParameter, _pCliPackages: Array<string>, pProjectHandler: Project): Promise<void> {
+        const lConsole = new Console();
+
+        // Cli parameter.
+        const lPackageName: string = <string>pParameter.parameter.get('package_name');
+
+        // Construct paths.
+        const lPackage = pProjectHandler.getPackageConfiguration(lPackageName);
+        const lBaseFileDirectory = path.resolve(__dirname, '..', '..', 'page-files'); // called from library/source
+        const lPackageScratchpadDirectory = path.resolve(lPackage.directory, 'page');
+
+        // Copy page blueprint. No overrides.
+        lConsole.writeLine('Initialize page files...');
+        FileUtil.copyDirectory(lBaseFileDirectory, lPackageScratchpadDirectory, false);
+
+        // Run build command.
+        const lBuildCommand: BuildCommand = new BuildCommand();
+        await lBuildCommand.build({
+            projectHandler: pProjectHandler,
+            packgeName: lPackage.packageName,
+            pack: true,
+            target: 'web',
+            scope: 'main',
+            buildType: 'page',
+            serve: !pParameter.parameter.has('build-only')
+        });
+    }
+}
