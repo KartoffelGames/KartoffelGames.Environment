ff69c6c7f664744bdc771f2aa2f8d2bd775baf82
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-12-22 04:36:34 +0100

    Dont use index file as bin, renamed and moved bin code of environment.

---
+++ environment/packages/kartoffelgames.environment/source/cli.ts
@@ -0,0 +1,22 @@
+import { Process, ExecutionContext, Package, ProcessParameter } from '@kartoffelgames/environment.core';
+
+// TODO: Deno-rework
+(() => {
+    // Find kg cli path. 
+    const lKgCliPath: string = Package.resolveToPath('@kartoffelgames/environment.cli/library/source/cli.js');
+
+    // Get all command parameter and find index of starting kg parameter.
+    const lCommandParameter: Array<string> = ExecutionContext.parameters;
+    const lKgStartCommandIndex: number = lCommandParameter.findIndex((pParameter) => {
+        return pParameter.endsWith('index.js');
+    });
+
+    // Array of only kg parameters.
+    const lKgCommandParts: Array<string> = lCommandParameter.slice(lKgStartCommandIndex + 1);
+
+    // Create process parameter.
+    const lProcessParameter: ProcessParameter = new ProcessParameter(ExecutionContext.workingDirectory, ['node', lKgCliPath, ...lKgCommandParts]);
+
+    // Execute command in current working directory.
+    new Process().executeInConsole(lProcessParameter);
+})();
