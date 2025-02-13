b37e9932f8c795563694bf1f8dc748dfb8b77fe1
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2025-02-13 23:45:08 +0100

    Skip npm and github release when no package json exists.

--- packages/kartoffelgames.environment.command_transform/source/command.ts
+++ packages/kartoffelgames.environment.command_transform/source/command.ts
@@ -1,5 +1,5 @@
 import { CliCommandDescription, CliParameter, ICliPackageCommand, Package, Project, FileSystem } from '@kartoffelgames/environment-core';
-import { build, emptyDir } from "@deno/dnt";
+import { build } from "@deno/dnt";
 
 export class KgCliCommand implements ICliPackageCommand<TransformConfiguration> {
     /**
@@ -10,17 +10,13 @@ export class KgCliCommand implements ICliPackageCommand<TransformConfiguration> 
             command: {
                 description: 'Transform packages into another runtime.',
                 parameters: {
-                    root: 'transform',
-                    optional: {
-                        nodejs: {
-                            shortName: 'n'
-                        }
-                    }
+                    root: 'transform'
                 }
             },
             configuration: {
                 name: 'transform',
                 default: {
+                    enableNode: false,
                     nodeDirectory: './node'
                 }
             }
@@ -33,7 +29,7 @@ export class KgCliCommand implements ICliPackageCommand<TransformConfiguration> 
      * @param _pParameter - Command parameter.
      * @param pCommandPackages - All cli packages grouped by type.
      */
-    public async run(pProject: Project, pPackage: Package | null, pParameter: CliParameter): Promise<void> {
+    public async run(pProject: Project, pPackage: Package | null, _pParameter: CliParameter): Promise<void> {
         // Needs a package to run page.
         if (pPackage === null) {
             throw new Error('Package to run page not specified.');
@@ -43,7 +39,7 @@ export class KgCliCommand implements ICliPackageCommand<TransformConfiguration> 
         const lConfiguration: TransformConfiguration = pPackage.cliConfigurationOf(this);
 
         // Transform to node when specified.
-        if (pParameter.has('nodejs')) {
+        if (lConfiguration.enableNode) {
             const lNodeDirectory = FileSystem.pathToAbsolute(pPackage.directory, lConfiguration.nodeDirectory);
             await this.transformToNode(pProject, pPackage, lNodeDirectory);
         }
@@ -55,22 +51,70 @@ export class KgCliCommand implements ICliPackageCommand<TransformConfiguration> 
         // Clean old node transform.
         FileSystem.emptyDirectory(pNodeDirectory);
 
-        // Find all files in source.
-        const lSourceFiles = FileSystem.findFiles(pPackage.sourcreDirectory, { include: { extensions: ['ts'] } });
+        // Find all files used in the export deno.json property.
+        const lExportedFiles: Array<string> = new Array<string>();
+        if (typeof pPackage.configuration["exports"] === 'string') {
+            lExportedFiles.push(FileSystem.pathToAbsolute(pPackage.directory, pPackage.configuration["exports"]));
+        } else {
+            for (const lExportPath of Object.values(pPackage.configuration["exports"] ?? {})) {
+                // export path must be a string.
+                if (typeof lExportPath !== 'string') {
+                    continue;
+                }
+
+                lExportedFiles.push(FileSystem.pathToAbsolute(pPackage.directory, lExportPath));
+            }
+        }
+
+        // Store any file that is not exported so it is included in the installed package.
+        const lPublishedFiles: Array<string> = new Array<string>();
+
+        // Filter all none ts files from exported file list.
+        for (let i = lExportedFiles.length - 1; i > -1; i--) {
+            const lExportedFilePath: string = lExportedFiles[i];
+            const lExportedFileInformation = FileSystem.pathInformation(lExportedFilePath);
+
+            console.log(lExportedFileInformation.extension)
+
+            if(lExportedFileInformation.extension !== '.ts') {
+                // Remove it from the exported files.
+                lExportedFiles.splice(i, 1);
+
+                // Add it to the published files.
+                lPublishedFiles.push(lExportedFilePath);
+            }
+        }
+
+        // Convert all exported files to relative paths.
+        const lRelativeExportedFiles: Array<string> = lExportedFiles.map((pFilePath: string) => {
+            return FileSystem.pathToRelative(Deno.cwd(), pFilePath);
+        });
+
+        // Convert all published files into relative paths with the package directory as root.
+        const lRelativePublishedFiles: Array<string> = lPublishedFiles.map((pFilePath: string) => {
+            return FileSystem.pathToRelative(pPackage.directory, pFilePath);
+        });
 
         await build({
             importMap: FileSystem.pathToAbsolute(pPackage.directory, 'deno.json'),
-            entryPoints: lSourceFiles,
+            entryPoints: lRelativeExportedFiles,
             outDir: pNodeDirectory,
             shims: {
-                // see JS docs for overview and more options
+                // Shim anything available.
                 deno: true,
+                timers: true,
+                prompts: true,
+                blob: true,
+                crypto: true,
+                domException: true,
+                undici: true,
+                webSocket: true
             },
             package: {
                 // package.json properties
-                name: "your-package",
-                version: Deno.args[0],
-                description: "Your package.",
+                name: pPackage.configuration.name,
+                version: pPackage.configuration.version,
+                description: pPackage.id,
                 license: "MIT",
                 repository: {
                     type: "git",
@@ -94,5 +138,6 @@ export class KgCliCommand implements ICliPackageCommand<TransformConfiguration> 
 }
 
 type TransformConfiguration = {
+    enableNode: boolean;
     nodeDirectory: string;
 };
