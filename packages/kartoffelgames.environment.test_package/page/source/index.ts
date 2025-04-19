cb8de2cee28178ed7534e783afe64c1d2f4e95b4
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2025-04-19 23:35:40 +0200

    Remove test package.

--- packages/kartoffelgames.environment.test_package/page/source/index.ts
+++
@@ -1,6 +0,0 @@
-import { Console } from "@kartoffelgames/environment-core";
-import WorkerCode from '../../library/Kartoffelgames.Environment.Test_Package.jsworker';
-
-new Console().banner('Hello World!!!');
-
-const myWorker = new Worker(WorkerCode);
--- packages/kartoffelgames.environment.test_package/source/index.ts
+++
@@ -1,3 +0,0 @@
-import { Console } from "@kartoffelgames/environment-core";
-
-new Console().banner('Called from INNER');
